d002fd57b9aca42ded8fca1166a2fa2b32eedd8d
Carsmaniac <mickledapickle@me.com>
2018-12-10 11:49:26 +1100

    Update man.py to reflect new/updated functions
    man.py now grabs accessory_names and accessories, and passes all the now
    needed variables to the functions in paintjob.py
    
    paintjob_new.py added to gitignore is a temporarily file I was using to
    store new versions of functions (added in 8183b40 and 6419858) and other
    things I'm testing with new_truck_format

--- man.py
+++ man.py
@@ -26,8 +26,12 @@ pack_author = manual["Params"]["pack_author"]
 
 database_name = manual["Params"]["database_name"]
 vehicle_type = "euro"
-new_truck_format = False
-# TODO: vehicle_type (if need be) and new_truck_format support
+new_truck_format = manual["Params"].getboolean("new_truck_format")
+# TODO: vehicle_type (if need be)
+
+if new_truck_format:
+    accessory_names = manual["Params"][accessory_names].split(",")
+    accessories = dict(item.split("=") for item in manual["Params"][accessories].split(","))
 
 print("Generating paintjob for %s..." % database_name)
 
@@ -35,14 +39,16 @@ print("Creating folders...")
 paintjob.Folders.common_mod_folders()
 paintjob.Folders.specific_mod_folders(make=make, model=model, new_truck_format=new_truck_format)
 print("Generating definition files...")
-paintjob.Files.def_sii(make=make, model=model, cabins=cabins, internal_name=internal_name, ingame_name=ingame_name, price=price, unlock_level=unlock_level)
+paintjob.Files.def_sii(make=make, model=model, cabins=cabins, internal_name=internal_name, ingame_name=ingame_name, price=price, unlock_level=unlock_level, new_truck_format=new_truck_format)
+if new_truck_format:
+    paintjob.Files.def_accessory_sii(make=make, model=model, internal_name=internal_name, accessory_names=accessory_names, accessories=accessories)
 paintjob.Files.material_mat(internal_name=internal_name)
 paintjob.Files.generate_tobj_files(internal_name=internal_name, new_truck_format=new_truck_format)
 paintjob.Files.manifest_sii(pack_version=pack_version, pack_name=pack_name, pack_author=pack_author)
 print("Copying images...")
-paintjob.Files.copy_image_files(mode="man", internal_name=internal_name) # TODO: different file name for man skins, for easy save-compile-testing
+paintjob.Files.copy_image_files(truck_list="manual", internal_name=internal_name, new_truck_format=new_truck_format, make=make, model=models, accessory_names=accessory_names) # TODO: different file name for man skins, for easy save-compile-testing
 print("Copying package files...")
-paintjob.Files.copy_mod_package_files(mode="man")
+paintjob.Files.copy_mod_package_files(truck_list="manual")
 print("Compiling mod...")
 paintjob.compile_mod_file()
 print("Cleaning up...")
