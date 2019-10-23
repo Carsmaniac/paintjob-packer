9608c575015da34cc9e4b8501a28426cd61b1e64
Carsmaniac <mickledapickle@me.com>
2019-10-23 11:59:13 +1100

    Add common mod and paintjob files
    Program now generates files that are mod-specific (manifest.sii and
    mod_manager_image.jpg) and paintjob-specific (icon files). All other
    files including those in /def and /vehicle are dependent on the vehicles
    within each paintjob, and will be implemented next.

--- library/paintjob.py
+++ library/paintjob.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, binascii, codecs
 
 class Vehicle:
     def example_variables():
@@ -38,10 +38,60 @@ def make_folder(path):
     if not os.path.exists("output/" + path):
         os.makedirs("output/" + path)
 
+def convert_string_to_hex(string_input):
+    if isinstance(string_input, int):
+        string_input = bytes([string_input])
+    elif isinstance(string_input, str):
+        string_input = string_input.encode()
+    string_output = binascii.hexlify(string_input)
+    string_output = string_output.decode()
+    return string_output
+
+def generate_tobj(path):
+    tobj_string = "010AB170000000000000000000000000000000000100020002000303030002020001000000010000"
+    tobj_string += convert_string_to_hex(len(path))
+    tobj_string += "00000000000000"
+    tobj_string += convert_string_to_hex(path)
+    tobj_file = codecs.decode(tobj_string, "hex_codec")
+    return tobj_file
+
+def make_manifest_sii(mod_version, mod_name):
+    file = open("output/manifest.sii", "w")
+    file.write("SiiNunit\n")
+    file.write("{\n")
+    file.write("mod_package: .package_name\n")
+    file.write("{\n")
+    file.write("    package_version:  \"%s\"\n" % mod_version)
+    file.write("    display_name:     \"%s\"\n" % mod_name)
+    file.write("    author:           \"Carsmaniac\"\n")
+    file.write("\n")
+    file.write("    category[]:       \"paint_job\"\n")
+    file.write("\n")
+    file.write("    icon:             \"mod_manager_image.jpg\"\n")
+    file.write("    description_file: \"mod_manager_description.txt\"\n")
+    file.write("}\n")
+    file.write("}\n")
+    file.close()
+
+def copy_mod_manager_image():
+    shutil.copyfile("library/placeholder files/mod_manager_image.jpg", "output/mod_manager_image.jpg")
+
 def make_material_folder():
     make_folder("material/ui/accessory/paintjob_icons")
 
-def get_ini_list(list_string):
-    out_list = list_string.split(",")
-    for item in out_list:
-        pass
+def copy_paintjob_icon(pj_int_name):
+    shutil.copyfile("library/placeholder files/paintjob_icon.dds", "output/material/ui/accessory/paintjob_icons/%s_icon.dds" % pj_int_name)
+
+def make_paintjob_icon_tobj(pj_int_name):
+    file = open("output/material/ui/accessory/paintjob_icons/%s_icon.tobj" % pj_int_name, "wb")
+    file.write(generate_tobj("/material/ui/acessory/paintjob_icons/%s_icon.dds" % pj_int_name))
+    file.close()
+
+def make_paintjob_icon_mat(pj_int_name):
+    file = open("output/material/ui/accessory/paintjob_icons/%s_icon.mat" % pj_int_name, "w")
+    file.write("material: \"ui\"\n")
+    file.write("{\n")
+    file.write("    texture:      \"%s_icon.tobj\"\n" % pj_int_name)
+    file.write("    texture_name: \"texture\"\n")
+    file.write("}\n")
+    file.close()
--- make new pack.py
+++ make new pack.py
@@ -19,3 +19,34 @@ mod_link = input_file["pack info"]["link"]
 description = input_file["pack info"]["description"]
 more_info = input_file["pack info"]["more info"]
 suggested_by = input_file["pack info"]["suggested by"].split(",")
+
+clear_output_folder()
+
+make_manifest_sii(mod_version, mod_name)
+copy_mod_manager_image()
+make_material_folder()
+
+for pj in list_of_paintjobs:
+    if pj == main_paintjob:
+        pj_main = True
+        pj_all_vehicles = False
+    else:
+        pj_main = False
+    pj_int_name = "cm_"+pj
+    pj_name = input_file[pj]["name"]
+    pj_price = input_file[pj]["price"]
+    pj_colour = input_file[pj]["main colour"]
+    if not pj_main:
+        pj_all_vehicles = input_file[pj].getboolean("all vehicles")
+    pj_list_of_vehicles = []
+    if pj_all_vehicles:
+        for veh in list(input_file[main_painjob].keys()):
+            if veh not in ("name", "price", "main colour", "all vehicles"):
+                pj_list_of_vehicles.append(veh)
+    else:
+        for veh in list(input_file[pj].keys()):
+            if veh not in ("name", "price", "main colour", "all vehicles"):
+                pj_list_of_vehicles.append(veh)
+    copy_paintjob_icon(pj_int_name)
+    make_paintjob_icon_tobj(pj_int_name)
+    make_paintjob_icon_mat(pj_int_name)
