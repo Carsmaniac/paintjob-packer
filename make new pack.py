5585b98e28b13ae486999138d74f73766bd172ae
Carsmaniac <mickledapickle@me.com>
2019-10-24 17:34:34 +1100

    Get vehicle and cabin variables
    Each vehicle is iterated through, and its variables are read from its
    .ini file. From here vehicle-specific files can be made.

--- make new pack.py
+++ make new pack.py
@@ -50,3 +50,32 @@ for pj in list_of_paintjobs:
     copy_paintjob_icon(pj_int_name)
     make_paintjob_icon_tobj(pj_int_name)
     make_paintjob_icon_mat(pj_int_name)
+
+    pj_contains_truck = False
+    pj_contains_trailer = False
+    for veh in pj_list_of_vehicles:
+        veh_ini = configparser.ConfigParser(allow_no_value = True)
+        veh_ini.read("vehicles/%s/%s.ini" % (game, veh))
+        veh_make = veh_ini["vehicle info"]["make"]
+        veh_model = veh_ini["vehicle info"]["model"]
+        veh_path = veh_ini["vehicle info"]["vehicle path"]
+        veh_alt_uvset = veh_ini["vehicle info"].getboolean("alt uvset")
+        veh_name = veh_ini["vehicle info"]["name"]
+        veh_trailer = veh_ini["vehicle info"].getboolean("trailer")
+        veh_mod = veh_ini["vehicle info"].getboolean("mod")
+        if veh_mod:
+            veh_mod_authot = veh_ini["vehicle info"]["mod author"]
+            veh_mod_link = veh_ini["vehicle info"]["mod link"]
+        veh_uses_accessories = veh_ini["vehicle info"].getboolean("uses accessories")
+        if veh_uses_accessories:
+            veh_accessories = veh_ini["vehicle info"]["accessories"].split(",")
+            veh_acc_dict = {}
+            for acc in veh_accessories:
+                veh_acc_dict[acc] = list(veh_ini[acc].keys())
+        if veh_trailer:
+            pj_contains_trailer = True
+        else:
+            pj_contains_truck = True
+            veh_separate_paintjobs = veh_ini["cabins"].getboolean("separate paintjobs")
+            veh_cabins = dict(veh_ini["cabins"].items())
+            veh_cabins.pop("separate paintjobs", None)
