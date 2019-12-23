2f2d4d4cfa7ea5f470ef1f72d7bc159583e002dd
Carsmaniac <mickledapickle@me.com>
2019-12-23 12:52:49 +1100

    Output unifier progress
    The unifier now prints the paintjob it's working on, as well as the
    total number of individual paintjobs before and after operation. A few
    other lines have been modified as it turns out in the creation of the
    unifier.ini, each paintjob's int_name already has the cm_ prefix.

--- library/placeholder files/unifier.py
+++ library/placeholder files/unifier.py
@@ -8,6 +8,7 @@ paintjobs_after_unifying = 0
 
 paintjobs = list(uni_ini["paintjobs"].keys())
 for pj in paintjobs:
+    print("Unifying "+pj)
     vehicles = list(uni_ini[pj].keys())
     for veh in vehicles:
         path = uni_ini[veh]["path"]
@@ -18,19 +19,19 @@ for pj in paintjobs:
         for cab_size in orig_cab_sizes:
             cab_name = uni_ini[veh][cab_size]
             cab_dict[cab_size] = cab_name
-            file = open("vehicle/truck/upgrade/paintjob/cm_%s/%s/cabin_%s.tobj" % (pj, veh, cab_size), "rb")
+            file = open("vehicle/truck/upgrade/paintjob/%s/%s/cabin_%s.tobj" % (pj, veh, cab_size), "rb")
             this_tobj = str(file.read())[-6:-5]
             file.close()
             if this_tobj not in tobj_cab_sizes:
                 tobj_cab_sizes[this_tobj] = []
             tobj_cab_sizes[this_tobj].append(cab_size)
-            os.remove("def/vehicle/truck/%s/paint_job/cm_%s_%s.sii" % (path, pj, cab_size))
+            os.remove("def/vehicle/truck/%s/paint_job/%s_%s.sii" % (path, pj, cab_size))
             if accessories and cab_size not in tobj_cab_sizes:
-                os.remove("def/vehicle/truck/%s/paint_job/accessory/cm_%s_%s.sii" % (path, pj, cab_size))
+                os.remove("def/vehicle/truck/%s/paint_job/accessory/%s_%s.sii" % (path, pj, cab_size))
+            paintjobs_before_unifying += 1
 
         for cab_size in tobj_cab_sizes:
-            pass # make sii
-            cab_pj_name = "cm_%s_%s" % (pj, cab_size)
+            cab_pj_name = "%s_%s" % (pj, cab_size)
             file = open("def/vehicle/truck/%s/paint_job/%s.sii" % (path, cab_pj_name), "w")
             file.write("SiiNunit\n")
             file.write("{\n")
@@ -44,3 +45,6 @@ for pj in paintjobs:
             file.write("}\n")
             file.write("}\n")
             file.close()
+            paintjobs_after_unifying += 1
+print("")
+print("Total paintjobs reduced from %s to %s (reduction of %s%)" % (paintjobs_before_unifying, paintjobs_after_unifying, 100*(1-(paintjobs_after_unifying / paintjobs_before_unifying))))
