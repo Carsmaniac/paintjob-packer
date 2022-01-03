4c657b541b3e7834f855c3e8ee1e412966100271
Carsmaniac <mickledapickle@me.com>
2022-01-03 17:04:37 +1100

    Create Windows build script
    A script has been written to build Paint Job Packer for Windows. It
    packages the program into an .exe using PyInstaller, then turns the .exe
    and its program files into an installer using Inno Setup. Two versions
    are created, one that includes templates and one that doesn't.

---
+++ build/build-windows.py
@@ -0,0 +1,49 @@
+import os
+import shutil
+import PyInstaller.__main__
+
+INNO_SETUP_PATH = "C:/Program Files (x86)/Inno Setup 6/ISCC.exe"
+
+# Get the current PJP version
+version = ""
+version_ini = open("../library/version.ini", "r")
+for line in version_ini.readlines():
+    if line.startswith("installed version"):
+        version = line.replace("installed version: ", "").rstrip()
+version_ini.close()
+
+# Update the Inno Setup script with the current version
+script_read = open("inno-script.iss", "r")
+script_lines = script_read.readlines()
+script_read.close()
+
+script_write = open("inno-script.iss", "w")
+for line in script_lines:
+    if "#define MyAppVersion" in line:
+        line = "#define MyAppVersion \"{}\"\n".format(version)
+    script_write.write(line)
+script_write.close()
+
+# Package into .exe and create installer, without templates
+PyInstaller.__main__.run(["../packer.py", "--onedir", "--windowed",
+    "--icon=../library/packer images/icon.ico", "--name=Paint Job Packer",
+    "--add-data=../library;library"])
+os.system("\"{}\" inno-script.iss".format(INNO_SETUP_PATH))
+
+shutil.make_archive("paint-job-packer-v{}-windows-no-templates".format(version), "zip", "output")
+shutil.rmtree("build")
+shutil.rmtree("dist")
+shutil.rmtree("output")
+os.remove("Paint Job Packer.spec")
+
+# Package into .exe and create installer, with templates
+PyInstaller.__main__.run(["../packer.py", "--onedir", "--windowed",
+    "--icon=../library/packer images/icon.ico", "--name=Paint Job Packer",
+    "--add-data=../library;library", "--add-data=../templates;templates"])
+os.system("\"{}\" inno-script.iss".format(INNO_SETUP_PATH))
+
+shutil.make_archive("paint-job-packer-v{}-windows-templates".format(version), "zip", "output")
+shutil.rmtree("build")
+shutil.rmtree("dist")
+shutil.rmtree("output")
+os.remove("Paint Job Packer.spec")
